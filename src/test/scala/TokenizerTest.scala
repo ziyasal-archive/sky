53ec03623c4ec3779c18bd297e5a354f068be1cd
Ziya SARIKAYA <ziya.sarikaya@here.com>
2016-12-27 13:55:25 +0100

    add tokenize test

--- src/test/scala/TokenizerTest.scala
+++ src/test/scala/TokenizerTest.scala
@@ -1,5 +1,13 @@
+import lib.Tokenizer
 import org.scalatest._
 
 class TokenizerTest extends FlatSpec with Matchers {
+
+  "A Tokenizer" should "tokenize given code" in {
+    val tokenizer = new Tokenizer()
+    val tokens = tokenizer.tokenize("(add 2 (subtract 4 2))")
+
+    tokens.length should be (9)
+  }
 
 }
